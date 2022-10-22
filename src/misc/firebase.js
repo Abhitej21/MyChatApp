468d89eeee9451cba150c44ca7aaa2ff1a13903e
Abhi <abhisamudrala21@gmail.com>
2022-10-22 16:53:12 +0530

    Initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBf8jd1ZWHCh_T8IquKKK-FyIzKNbn52w4",
+    authDomain: "abhi-chat-app-7fc00.firebaseapp.com",
+    projectId: "abhi-chat-app-7fc00",
+    storageBucket: "abhi-chat-app-7fc00.appspot.com",
+    messagingSenderId: "978789082901",
+    appId: "1:978789082901:web:9577242db0bfd8fad22b1c"
+  };
+
+  const app = firebase.initializeApp(config);
+
+  
