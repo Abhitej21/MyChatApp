9ce15fe5dbb94473181e551fed8bb2f394809e52
Abhi <abhisamudrala21@gmail.com>
2022-11-03 18:10:06 +0530

    added status showing badge and message functionality

---
+++ src/components/Presence.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+import React from 'react'
+import { Badge, Tooltip, Whisper } from 'rsuite';
+import { usePresence } from '../misc/custom-hooks'
+
+
+const getColor = (presence) => {
+    if(!presence){
+        return 'gray';
+    }
+    switch(presence.state){
+        case "online": return 'green';
+        case "offline": return 'red';
+        default: return 'gray';
+    }
+};
+
+
+const getText = (presence) => {
+    if(!presence){
+        return 'State unknown';
+    }
+    return presence.state==='online'?'Online':`Last seen ${new Date(presence.last_changed).toDateString()}`;
+}
+
+function Presence({uid}) {
+
+
+    const presence = usePresence(uid);
+    console.log(presence);
+    return (
+        <Whisper placement="top" trigger="hover" speaker={
+            <Tooltip>
+                {getText(presence)}
+            </Tooltip>
+        }>
+            <Badge className='cursor-pointer' style={{backgroundColor: getColor(presence)}}/>
+        </Whisper>
+    )
+}
+
+export default Presence;
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,8 +1,21 @@
+/* eslint-disable spaced-comment */
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable react/function-component-definition */
 import React,{createContext,useContext,useEffect,useState} from 'react';
+import firebase from 'firebase';
 import { auth, database } from '../misc/firebase';
+
+
+export const isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+    state: 'online',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 
 const ProfileContext = createContext();
@@ -13,9 +26,11 @@ export const ProfileProvider = ({children}) => {
     const [isLoading,setIsLoading] = useState(true);
     useEffect(() => {
         let userRef;
+        //for showing status like online or offline userStatusRef is used
+        let userStatusRef;
         const authUnsub = auth.onAuthStateChanged(authObj => {
             if(authObj){
-
+                userStatusRef = database.ref(`/status/${authObj.uid}`);
 
                 userRef = database.ref(`/profiles/${authObj.uid}`);
                 userRef.on('value',(snap) =>{
@@ -30,16 +45,47 @@ export const ProfileProvider = ({children}) => {
                     setProfile(data);
                     setIsLoading(false);
                 });
+                
+                
+
+                database.ref('.info/connected').on('value', (snapshot) => {
+                    // If we're not currently connected, don't do anything.
+                    if (snapshot.val() === false) {
+                        return;
+                    };
+                
+                    // If we are currently connected, then use the 'onDisconnect()' 
+                    // method to add a set which will only trigger once this 
+                    // client has disconnected by closing the app, 
+                    // losing internet, or any other means.
+                    userStatusRef.onDisconnect().set(isOfflineForDatabase).then(() => {
+                        // The promise returned from .onDisconnect().set() will
+                        // resolve as soon as the server acknowledges the onDisconnect() 
+                        // request, NOT once we've actually disconnected:
+                        // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
+                
+                        // We can now safely set ourselves as 'online' knowing that the
+                        // server will mark us as offline once we lose connection.
+                        userStatusRef.set(isOnlineForDatabase);
+                    });
+                });
+
+
             }else{
                 if(userRef){
                     userRef.off();
                 }
+                if(userStatusRef){ 
+                    userStatusRef.off();
+                }
+                database.ref(`.info/connected`).off(); 
                 setProfile(null);
                 setIsLoading(false);
             }
         });
         return () => {
             authUnsub();
+            database.ref(`.info/connected`).off();
             if(userRef){
                 userRef.off();
             }
